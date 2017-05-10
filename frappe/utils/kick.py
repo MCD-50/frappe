f93d4d14a36446251ed4c05aec0f567ffdf4a1db
Ayush AS <ayush.as.shukla@gmail.com>
2017-05-10 17:51:14 +0530

    Added fetch data

--- frappe/utils/kick.py
+++ frappe/utils/kick.py
@@ -3,6 +3,9 @@ import frappe
 import os
 import json
 from frappe import conf
+from frappe.utils import today, getdate, add_to_date
+from frappe.desk.form.load import get_communication_data
+
 
 
 @frappe.whitelist()
@@ -11,20 +14,41 @@ def get_dev_port():
 
 @frappe.whitelist()
 def get_issues(obj):
-	# convert to dict
-	if obj:
-		obj = frappe._dict(obj)
-	
+	obj = frappe._dict(obj)
 
-	return None
+	from_last_week = add_to_date(getdate(today()), days=-7, as_string=True)
+	issues = frappe.get_list('Issue', fields="*", filters={"modified":(">", from_last_week)})
+	return issues
 
 @frappe.whitelist()
 def get_opportunities(obj):
-	# convert to dict
-	if obj:
-		obj = frappe._dict(obj)
+	obj = frappe._dict(obj)
+
+	from_last_month = add_to_date(getdate(today()), days=-30, as_string=True)
+	opportunity = frappe.get_list('Opportunity', fields="*", filters={"modified":(">", from_last_month)})
+	return opportunity
+
+@frappe.whitelist()
+def get_communications(obj):
+	# self, doctype, name, limit_start, length, communications= [], after = None
+	"""get get_communication_data returns only 20 items at a time"""
+	obj = frappe._dict(obj)
 	
+	communications = []
+	start = obj.limit_start
+	x = get_communication_data(obj.doctype, obj.name, start, 20, obj.after)
+	while x is not None:
+		for c in x:
+			if c.communication_type=="Communication":
+				c.attachments = json.dumps(frappe.get_all("File", fields=["file_url", "is_private"],
+				filters={"attached_to_doctype": "Communication", "attached_to_name": c.name}))
 
-	filters = {}
-	return None
-
+			elif c.communication_type=="Comment" and c.comment_type=="Comment":
+				c.content = frappe.utils.markdown(c.content)
+		communications += x
+		if len(x) > 19:
+			start = start + 20
+			x = get_communication_data(obj.doctype, obj.name, start, 20, obj.after)
+		else:
+			x = None
+	return communications
