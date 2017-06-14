cb4eea1c45146649617b8a42b89c6978e9fc73e9
MCD-50 <ayush.as.shukla@gmail.com>
2017-06-14 16:22:58 +0530

    Added frappe-node

---
+++ frappe/utils/frappe_node.py
@@ -0,0 +1,75 @@
+from __future__ import unicode_literals
+import frappe
+import frappe.utils
+import json
+
+
+@frappe.whitelist()
+def add_doc(data):
+	"""Returns a new document of the given DocType with provided values.
+
+	:param data.doctype: DocType of the new document.
+	:param data.info: Dict to facilitate add operation.
+	"""
+	obj = get_dict_obj(data)
+	doctype = obj.get('doctype')
+	info = obj.get('info')
+	
+	x = frappe.new_doc(doctype, info.get('parent_doc'),info.get('parentfield'))
+	
+	return x
+
+
+@frappe.whitelist()
+def delete_doc(data):
+	"""Delete a document..
+
+	:param data.doctype: DocType of document to be delete.
+	:param data.info: Dict to facilitate delete operation.
+	"""
+	obj = get_dict_obj(data)
+
+
+@frappe.whitelist()
+def update_doc(data):
+	"""Returns a updated document of the given DocType with provided info.
+
+	:param data.doctype: DocType of document to be update.
+	:param data.info: Dict to facilitate update operation.
+	"""
+	obj = get_dict_obj(data)
+
+
+@frappe.whitelist()
+def get_doc(data):
+	"""Returns a document if exists else create new document of the given DocType with provided info.
+
+	:param data.doctype:DocType of the new or existing document.
+	:param data.info: Dict to facilitate get operation.
+	"""
+	obj = get_dict_obj(data)
+
+
+@frappe.whitelist()
+def get_list(data):
+	"""Returns list of document of the given DocType.
+
+	:param data.doctype:DocType of documents to be fetched.
+	:param data.info: Dict to facilitate list operation.
+	"""
+	obj = get_dict_obj(data)
+
+
+@frappe.whitelist()
+def get_meta(data):
+	"""Returns meta of document of the given DocType.
+
+	:param data.doctype:DocType of document.
+	:param data.info: Dict to facilitate meta operation.
+	"""
+	obj = get_dict_obj(data)
+
+
+def get_dict_obj(data):
+	obj = json.loads(data.decode('string-escape').strip('"'))
+	return frappe._dict(obj)
